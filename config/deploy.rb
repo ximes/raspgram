dc5a566dc67d55fc1c2375784d9f1f83ed9d376c
Stefano Franzin <stefano.franzin@seesaw.it>
2015-07-26 09:38:26 +0100

    Deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,26 +9,27 @@ set :rack_env, :production
 set :rvm_type, :system
 
 #general info
-set :user, 'pi'
+set :user, 'deploy'
 set :domain, '192.168.0.16'
 set :applicationdir, "/srv/raspgram"
 set :scm, 'git'
 set :application, "raspgram"
 set :repository,  "git@github.com:ximes/raspgram.git"
-set :branch, 'master'
+set :branch, 'develop'
 set :git_shallow_clone, 1
 set :scm_verbose, true
 set :deploy_via, :remote_cache
 
 role :web, domain                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
-role :db,  domain, :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 #deploy config
 set :deploy_to, applicationdir
 set :deploy_via, :export
 
-set :shared_children, fetch(:shared_children) + %w{statuses}
+set :shared_children, fetch(:shared_children) + %w{lib/tg log config/raspgram.yml}
+
+#set :linked_files, fetch(:linked_files) + %w{config/raspgram.yml}
 
 #addition settings. mostly ssh
 ssh_options[:forward_agent] = true
@@ -41,11 +42,6 @@ default_run_options[:pty] = true
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# After an initial (cold) deploy, symlink the app and restart nginx
-after "deploy:cold" do
-  admin.nginx_restart
-end
-
 # As this isn't a rails app, we don't start and stop the app invidually
 namespace :deploy do
   desc "Not starting as we're running passenger."
@@ -54,5 +50,11 @@ namespace :deploy do
 
   task :restart do
     run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "Override deploy:cold to NOT run migrations - there's no database"
+  task :cold do
+	update
+	start
   end
 end
