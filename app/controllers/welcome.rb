046d59af5762a482409ec916eb7814ec9f3fe585
Stefano Franzin <stefano.franzin@seesaw.it>
2015-08-04 21:01:49 +0100

    Removed old layout

--- app/controllers/welcome.rb
+++ app/controllers/welcome.rb
@@ -3,20 +3,4 @@ Raspgram::App.controllers :welcome do
     @admin_email = settings.admin_email
     render 'index'
   end
-
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   'Maps to url '/foo/#{params[:id]}''
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-  
-
 end
