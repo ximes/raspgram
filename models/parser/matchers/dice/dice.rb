3d5ea77aef600c6c76b2654cff1984e5df4756a1
Stefano Franzin <stefano.franzin@seesaw.it>
2016-01-08 21:04:30 +0000

    Dynamic dice size

--- models/parser/matchers/dice/dice.rb
+++ models/parser/matchers/dice/dice.rb
@@ -2,18 +2,23 @@ module Parser
 	class Dice < Matcher
 		def initialize
 			@word = "dice"
+			@option = 6
 		end
 		def help
 			" [size](optional)"
 		end
 		def parse(input, from)
+			options = input.match(/^([a-z]+)\s([0-9]+)$/i)
+			if options and options[1] == @word and options[2].to_i
+				@option = options[2].to_i
+			end
 			if /^#{@word}/i.match input
 				@input = input
 				@from = from
 			end
 		end
 		def callback
-			Telegram.msg "#{1+rand(6)}", @from
+			Telegram.msg "#{1+rand(@option)}", @from
 		end
 	end
 end
