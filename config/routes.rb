f09f5ed5cb6cfd5dc6a246d33d091f107a9a44b2
Stefano Franzin <stefano.franzin@seesaw.it>
2016-04-17 15:51:58 +0100

    Test api controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root 'homepage#index'
+  resource :updates, only: [:create], as: :api, path: 'api'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
