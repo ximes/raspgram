840cce6f40e60913f4e22a983b95385d24d72567
Stefano Franzin <stefano.franzin@seesaw.it>
2016-05-30 22:27:30 +0100

    dice trick fixup

--- db/migrate/20160530114659_add_dice_trick.rb
+++ db/migrate/20160530114659_add_dice_trick.rb
@@ -1,10 +1,10 @@
 class AddDiceTrick < ActiveRecord::Migration
 	def up
 		#add the dice trick to db
-		Trick.create(name: 'Dice', class_name: "dice", active: true, core: true)
+		Trick.create(name: 'Dice', class_name: "Dice", active: true, core: true)
 	end
 	def down
 		#remove dice trick from db
-		Trick.find_by(class_name: 'dice').destroy
+		Trick.find_by(class_name: 'Dice').destroy
 	end
 end
